623e6886b8eff6b0749ef41d1402c1da3261f52c
tanik <tanimoto@agileware.jp>
2019-04-02 17:45:02 +0900

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'register', to: 'users#new'
+  post 'users', to: 'users#create'
+  get 'connect', to: 'connections#create'
+  get 'state', to: 'connections#index'
+
+  root to: 'users#show'
 end
