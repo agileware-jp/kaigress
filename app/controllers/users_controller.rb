ea8db39d2db67b731280269a74da114957433f84
fkdkent <fkdkent@agileware.jp>
2019-04-03 17:39:34 +0900

    Create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to root_path
+    else
+      # TODO
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname)
   end
 end
