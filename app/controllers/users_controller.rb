7d4c9e466d94216d547ee302a5ee00e404902c13
fkdkent <fkdkent@agileware.jp>
2019-04-03 17:40:28 +0900

    Store uuid to session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      session[:uuid] = user.uuid
       redirect_to root_path
     else
       # TODO
