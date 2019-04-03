913049587f331c58dddf1eced5831fa8a6e674cc
Kevin Fischer <kevin@agileware.jp>
2019-04-03 18:35:20 +0900

    Connection validation

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,4 +1,12 @@
 class Connection < ApplicationRecord
   belongs_to :from, class_name: 'User', foreign_key: :from_id
   belongs_to :to, class_name: 'User', foreign_key: :to_id
+
+  validate :from_and_to_are_different
+
+  private
+
+  def from_and_to_are_different
+    errors.add(:base, :invalid) if from_id == to_id
+  end
 end
--- spec/models/connection_spec.rb
+++ spec/models/connection_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Connection, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#from_and_to_are_different' do
+    let(:user) { create :user }
+
+    it 'cannot create a connection between one and the same user' do
+      connection = build :connection, from: user, to: user
+      expect(connection).to be_invalid
+    end
+  end
 end
