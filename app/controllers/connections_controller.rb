0a106af79c14b27bd2b4f99308cb6a1c45392d78
Kevin Fischer <kevin@agileware.jp>
2019-04-03 18:35:31 +0900

    Connection Controller WIP

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,2 +1,11 @@
 class ConnectionsController < ApplicationController
+  before_action :set_user, only: %i[create]
+
+  def create
+    other = User.find_by!(uuid: params[:uuid])
+
+    @connection = @user.connect_to(other)
+
+    render 'users/show'
+  end
 end
