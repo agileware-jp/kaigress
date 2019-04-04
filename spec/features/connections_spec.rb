0de074a1afe86785c86c9f88df04b49a81cd0997
Kevin Fischer <kevin@agileware.jp>
2019-04-04 17:03:21 +0900

    Show simple error messages

--- spec/features/connections_spec.rb
+++ spec/features/connections_spec.rb
@@ -1,16 +1,45 @@
 require 'rails_helper'
 
+
+def register
+  visit register_path
+  fill_in 'Nickname', with: 'bob'
+  click_button
+end
+
+
 RSpec.feature 'Connection', type: :feature do
   describe 'Connection status' do
-    let(:user) { create :user }
     let(:other_user) { create :user }
 
+    before do
+      register
+      visit connect_path(uuid: uuid, connection_token: connection_token)
+    end
+
     context 'Connection successful' do
+      let(:uuid) { other_user.uuid }
+      let(:connection_token) { other_user.connection_token }
+
       it 'shows success message' do
+        expect(page).to have_text 'Connected with'
+      end
+
+      context 'when trying to connect one more time' do
+        it 'shows fail message' do
+          visit connect_path(uuid: uuid, connection_token: other_user.reload.connection_token)
+          expect(page).to have_text 'Connection failed'
+        end
       end
     end
 
-    context 'Connection failed' do
+    context 'Token is different' do
+      let(:uuid) { other_user.uuid }
+      let(:connection_token) { 'blabla' }
+
+      it 'shows fail message' do
+        expect(page).to have_text 'Connection failed'
+      end
     end
   end
 end
