9975ba66341645ffb70cb71e64dced5eaed49bcf
Kevin Fischer <kevin@agileware.jp>
2019-04-04 17:15:03 +0900

    Update QR code generation

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module UsersHelper
+  def qr_code_for(user)
+    qr_code_url = ERB::Util.url_encode(connect_url(uuid: user.uuid, connection_token: user.connection_token))
+    image_tag "https://chart.apis.google.com/chart?cht=qr&chs=300x300&chl=#{qr_code_url}"
+  end
+end
