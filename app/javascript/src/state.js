6a1f3492bcc081558fbc5cb3f8e39266135412b6
Kevin Fischer <kevin@agileware.jp>
2019-04-10 10:42:21 +0900

    Rewrite to webpack

---
+++ app/javascript/src/state.js
@@ -0,0 +1,76 @@
+import vis from 'visjs-network';
+
+const TEAMS = ['red', 'blue', 'green'];
+
+const container = document.getElementById('team-network');
+const data = {
+  nodes: new vis.DataSet(JSON.parse(container.dataset.users)),
+  edges: new vis.DataSet(JSON.parse(container.dataset.connections)),
+};
+const options = {
+  nodes: {
+    shape: 'dot',
+    font: {
+      size: 72,
+      face: 'Tahoma',
+    },
+  },
+  edges: {
+    width: 5,
+    color: { inherit: 'from' },
+    smooth: {
+        type: 'continuous',
+    },
+  },
+  physics: {
+    stabilization: true,
+    barnesHut: {
+        gravitationalConstant: -80000,
+        springConstant: 0.001,
+        springLength: 200,
+    },
+  },
+  interaction: {
+    dragNodes: false,
+    selectable: false,
+  },
+  groups: { // Copied from default groups
+    blue: {
+        color: { border: '#2B7CE9', background: '#97C2FC' },
+        font: { color: '#2B7CE9' },
+    },
+    red: {
+        color: { border: '#FA0A10', background: '#FB7E81' },
+        font: { color: '#FA0A10' },
+    },
+    green: {
+        color: { border: '#41A906', background: '#7BE141' },
+        font: { color: '#41A906' },
+    },
+  },
+};
+
+const points = {};
+
+const teamOfConnection = (c) => {
+    const fromUser = data.nodes.get({ filter: u => u.id === c.from })[0];
+    return fromUser.team;
+};
+
+const calcPoints = () => {
+    TEAMS.forEach(t => {
+    points[t] = data.edges.get({ filter: c => teamOfConnection(c) === t}).length;
+    });
+};
+
+const network = new vis.Network(container, data, options);
+
+App.cable.subscriptions.create({ channel: 'StateChannel', model_type: 'user' }, {
+    received: user => data.nodes.add(user),
+});
+
+App.cable.subscriptions.create({ channel: 'StateChannel', model_type: 'connection' }, {
+    received: connection => data.edges.add(connection),
+});
+
+calcPoints();
