4e757f7031fc685e80d7426fe89d7352222c7325
Kevin Fischer <kevin@agileware.jp>
2019-04-19 14:29:37 +0900

    Remove unneeded methods

--- app/models/team.rb
+++ app/models/team.rb
@@ -9,12 +9,4 @@ class Team
   def self.member_counts
     User.teams.keys.each_with_object({}) { |t, result| result[t] = User.send(t).count }.symbolize_keys
   end
-
-  def self.connections_of(team)
-    Connection.merge(User.where(team: team)).joins(:from)
-  end
-
-  def self.connections_by_team
-    User.teams.keys.to_h { |team| [team, connections_of(team)] }.symbolize_keys
-  end
 end
