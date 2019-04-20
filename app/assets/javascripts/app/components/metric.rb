c8033e15b36691f818bf1279a2050e52d52133a0
Kevin Fischer <kevin@agileware.jp>
2019-04-20 12:57:43 +0900

    Remove label

--- app/assets/javascripts/app/components/metric.rb
+++ app/assets/javascripts/app/components/metric.rb
@@ -3,10 +3,7 @@
 class Metric < Ferro::Component::Base
   class Value < Ferro::Component::Base; end
 
-  class Label < Ferro::Component::Base; end
-
   def before_create
-    @label = option_replace :label
     @value = option_replace :value
     @color = option_replace :color
   end
@@ -14,7 +11,6 @@ class Metric < Ferro::Component::Base
   def cascade
     add_child :metric_value, Value, content: @value
     metric_value.add_state @color, true if @color
-    # add_child :label, Label, content: @label
   end
 
   def metric_value=(new_value)
