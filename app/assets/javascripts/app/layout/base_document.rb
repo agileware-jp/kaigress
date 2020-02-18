e811576ee2cf95f7314394f929eb69dc90436d29
Kevin Fischer <kevin@agileware.jp>
2020-02-18 15:35:52 +0900

    Rename from rubykaigi2019-kaigress to kaigress

--- app/assets/javascripts/app/layout/base_document.rb
+++ app/assets/javascripts/app/layout/base_document.rb
@@ -7,7 +7,7 @@ class BaseDocument < Ferro::Document
   class CopyrightNotice < Ferro::Component::Base
     def cascade
       add_child :copyright, Ferro::Element::ExternalLink, content: 'Agileware Inc.', href: 'https://agileware.jp'
-      add_child :link, Ferro::Element::ExternalLink, content: 'Published on Github', href: 'https://github.com/agileware-jp/rubykaigi2019-kaigress'
+      add_child :link, Ferro::Element::ExternalLink, content: 'Published on Github', href: 'https://github.com/agileware-jp/kaigress'
     end
   end
 
